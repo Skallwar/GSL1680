191847745f56759a77854fc5b1518dbaee5683c1
ESTBLC <estblcsk@gmail.com>
2019-02-27 17:11:08 +0000

    Update to new Wire.h standard

--- GSL1680.cpp
+++ GSL1680.cpp
@@ -3,13 +3,19 @@
     @file     GSL1680.cpp
     @author   ESTBLC
     @section  HISTORY
-    
-    v1.0 - First release
+
+    v1.1.0 - Refactor and privatise some functions. Use new Wire standard
+    v1.0.0 - First release
 
     @thanks to wolfmanjm for the source code this lib is bassed on : https://github.com/wolfmanjm/GSL1680
 */
+#include <Arduino.h>
+#include <Wire.h>
 
-#include <GSL1680.h>
+#include <stdint.h>
+
+#include "GSL1680.h"
+#include "gslX680firmware.h"
 
 // Registres
 #define I2CADDR 0x40
@@ -17,96 +23,104 @@
 #define STATUS_REG 0xE0
 #define PAGE_REG 0xF0
 
-
 //CONST
 #define BUFSIZE 32
 
 struct Sts_event ts_event;
 
-GSL1680::GSL1680 () {
-    
+GSL1680::GSL1680()
+{
+
 }
 
-void GSL1680::begin(uint8_t WAKE, uint8_t INTRPT) {
-    Serial.println("Starting TS boot up"); 
+void GSL1680::begin(uint8_t WAKE, uint8_t INTRPT)
+{
+    Serial.println("GSL1680: Start boot up sequence");
     pinMode(WAKE, OUTPUT);          //
     digitalWrite(WAKE, LOW);        //
     pinMode(INTRPT, INPUT_PULLUP);  // Startup sequence PIN part
     delay(100);
-    Serial.println("Toggle Wake");
+
+    /* Serial.println("Toggle Wake"); */
 	digitalWrite(WAKE, HIGH);
 	delay(50);
 	digitalWrite(WAKE, LOW);
 	delay(50);
 	digitalWrite(WAKE, HIGH);
 	delay(30);
+
     Wire.begin();
 
     // CTP startup sequence
-	Serial.println("Clear reg");
+	/* Serial.println("Clear reg"); */
 	clear_reg();
-	Serial.println("Reset chip");
+	/* Serial.println("Reset chip"); */
 	reset();
-	Serial.println("Load FW");
+	/* Serial.println("GSL1680: Load FW"); */
 	loadfw();
 	//startup_chip();
-	Serial.println("Reset chip 2");
+	/* Serial.println("Reset chip 2"); */
 	reset();
-	Serial.println("Startup Chip");
+	/* Serial.println("Startup Chip"); */
     startchip();
-	Serial.println("Init Done");
+	Serial.println("GSL1680: Boot up complete");
 }
 
-void GSL1680::clear_reg() {
+void GSL1680::clear_reg()
+{
     uint8_t REG[4] = {0xE0, 0x80, 0xE4, 0xE0};
     uint8_t DATA[4] = {0x88, 0x01, 0x04, 0x00};
     uint8_t TIMER[4] = {20, 5, 5, 20};
 
-    for (int i = 0; i<=4; i++) {
-	    Wire.beginTransmission(I2CADDR);
-        Wire.send(REG[i]);
-        Wire.send(DATA[i]);
+    Wire.beginTransmission(I2CADDR);
+
+    int i;
+    for (i = 0; i < 4; ++i) {
+        Wire.write(REG[i]);
+        Wire.write(DATA[i]);
         delay(TIMER[i]);
-        int r = Wire.endTransmission();
-        if (r != 0){
-            Serial.print("i2c write error: "); Serial.print(r); Serial.print(" "); Serial.println(REG[i], HEX);
-        }
     }
+
+    int r = Wire.endTransmission();
+    if (r != 0){
+        Serial.print("i2c write error: "); Serial.print(r); Serial.print(" "); Serial.println(REG[i], HEX);
+    }
+
 }
 
-void GSL1680::reset() {
-    uint8_t REG[4] = {STATUS_REG, 0xE4};
-    uint8_t DATA[4] = {0x88, 0x04};
-    uint8_t TIMER[4] = {20, 10};
+void GSL1680::reset()
+{
+    uint8_t REG[2] = {STATUS_REG, 0xE4};
+    uint8_t DATA[2] = {0x88, 0x04};
+    uint8_t TIMER[2] = {20, 10};
 
-    for (int i = 0; i<=2; i++) {
-	    Wire.beginTransmission(I2CADDR);
-        Wire.send(REG[i]);
-        Wire.send(DATA[i]);
+    Wire.beginTransmission(I2CADDR);
+
+    int i;
+    for (i = 0; i < 2; ++i) {
+        Wire.write(REG[i]);
+        Wire.write(DATA[i]);
         delay(TIMER[i]);
-        int r = Wire.endTransmission();
-        if (r != 0){
-            Serial.print("i2c write error: "); Serial.print(r); Serial.print(" "); Serial.println(REG[i], HEX);
-        }
     }
 
-    DATA[0] = 0x00;
-    DATA[1] = 0x00;
-    DATA[2] = 0x00;
-    DATA[3] = 0x00;
-    datasend (0xBC, DATA, 4);
+    int r = Wire.endTransmission();
+    if (r != 0){
+        Serial.print("i2c write error: "); Serial.print(r); Serial.print(" "); Serial.println(REG[i], HEX);
+    }
+
+    uint8_t DATA_2[4] = {0};
+
+    datasend (0xBC, DATA_2, 4);
     delay(10);
-
 }
 
-void GSL1680::loadfw(){
+void GSL1680::loadfw()
+{
     uint8_t addr;
     uint8_t Wrbuf[4];
-    uint source_line = 0;
-    uint source_len = sizeof(GSLX680_FW) / sizeof(struct fw_data);
+    size_t source_len = sizeof(GSLX680_FW) / sizeof(struct fw_data);
 
-
-    for (source_line = 0; source_line < source_len; source_line++) {
+    for (size_t source_line = 0; source_line < source_len; source_line++) {
         addr = GSLX680_FW[source_line].offset;
         Wrbuf[0] = (char)(GSLX680_FW[source_line].val & 0x000000ff);
         Wrbuf[1] = (char)((GSLX680_FW[source_line].val & 0x0000ff00) >> 8);
@@ -117,38 +131,46 @@ void GSL1680::loadfw(){
     }
 }
 
-void GSL1680::startchip() {
+void GSL1680::startchip()
+{
     Wire.beginTransmission(I2CADDR);
+
     Wire.write(0xE0);   //Registre
     Wire.write(0x00);   //DATA
+
     int r = Wire.endTransmission();
     if (r != 0){
         Serial.print("i2c write error: "); Serial.print(r); Serial.print(" "); Serial.println(0xE0, HEX);
     }
 }
 
-void GSL1680::sleep() {
+void GSL1680::sleep()
+{
 
 }
 
-
-void GSL1680::datasend(uint8_t REG, uint8_t DATA[], uint16_t NB) {
+void GSL1680::datasend(uint8_t REG, uint8_t DATA[], uint16_t NB)
+{
     Wire.beginTransmission(I2CADDR);
+
     Wire.write(REG);
-    
-    for (uint16_t i=0; i<NB; i++) {
+
+    for (uint16_t i = 0; i < NB; i++) {
         Wire.write(DATA[i]);
     }
+
     int r = Wire.endTransmission();
-    if (r != 0){
+    if (r != 0) {
         Serial.print("i2c write error: "); Serial.print(r); Serial.print(" "); Serial.println(REG, HEX);
     }
-    return;
 }
 
-uint8_t GSL1680::dataread() {
+uint8_t GSL1680::dataread()
+{
     uint8_t TOUCHRECDATA[24] = {0};
+
     Wire.beginTransmission(I2CADDR);
+
     Wire.write(DATA_REG);
 
     int n = Wire.endTransmission();
@@ -171,18 +193,21 @@ uint8_t GSL1680::dataread() {
         ts_event.coords[i].Y = ( (((uint32_t)TOUCHRECDATA[(i*4)+7])<<8) | (uint32_t)TOUCHRECDATA[(i*4)+6] ) & 0x00000FFF;
         ts_event.coords[i].fingerID = (uint32_t)TOUCHRECDATA[(i*4)+7] >> 4; // finger that did the touch
     }
-    return ts_event.NBfingers;
 
+    return ts_event.NBfingers;
 }
 
-uint8_t GSL1680::readFingerID(int NB) {
+uint8_t GSL1680::readFingerID(int NB)
+{
     return ts_event.coords[NB].fingerID;
 }
 
-uint32_t GSL1680::readFingerX(int NB) {
+uint32_t GSL1680::readFingerX(int NB)
+{
     return ts_event.coords[NB].X;
 }
 
-uint32_t GSL1680::readFingerY(int NB) {
+uint32_t GSL1680::readFingerY(int NB)
+{
     return ts_event.coords[NB].Y;
 }
--- GSL1680.h
+++ GSL1680.h
@@ -3,32 +3,34 @@
     @file     GSL1680.h
     @author   ESTBLC
     @section  HISTORY
-    
+
+    v1.1.0 - Refactor and privatise some functions. Use new Wire standard
     v1.0 - First release
 
     @thanks to wolfmanjm for the source code this lib is bassed on : https://github.com/wolfmanjm/GSL1680
 */
-//#include <Arduino.h>
-#include <Wire.h>
-#include "gslX680firmware.h"
-#ifndef _GSL1680
-#define _GSL1680
+
+#ifndef GSL1680_H
+#define GSL1680_H
+
+#include <stdint.h>
 
 class GSL1680 {
- public:
-  GSL1680 ();
-  
-  void      begin(uint8_t WAKE, uint8_t INTRPT);
-  void      clear_reg();
-  void      reset();
-  void      loadfw();
-  void      startchip();
-  void      sleep();
-  void      datasend(uint8_t REG, uint8_t DATA[], uint16_t NB);
-  uint8_t       dataread();
-  uint8_t   readFingerID(int NB);
-  uint32_t  readFingerX(int NB);
-  uint32_t  readFingerY(int NB);
+    public:
+        GSL1680 ();
+        void      begin(uint8_t WAKE, uint8_t INTRPT);
+        uint8_t   dataread();
+        uint8_t   readFingerID(int NB);
+        uint32_t  readFingerX(int NB);
+        uint32_t  readFingerY(int NB);
+
+    private:
+        void      clear_reg();
+        void      reset();
+        void      loadfw();
+        void      startchip();
+        void      sleep();
+        void      datasend(uint8_t REG, uint8_t DATA[], uint16_t NB);
 };
 
 struct Scoords {
